830787027b3a537c0be2ed408e1a9105672b57c0
YukimiMelody <202024995+YukimiMelody@users.noreply.github.com>
2025-11-30 19:00:25 -0300

    Update settings.py
    Linting7

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -103,6 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': (
             'django.contrib.auth.password_validation.'
             'UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
